45e33fde8aa13dabc323267aa2a07d093685c7e3
Édouard Carré <edouard.carre.1@ulaval.ca>
2018-03-12 22:28:33 -0400

    Started toolbar and footer

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 
 @app.route('/about')
@@ -13,14 +13,24 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/signIn')
-def signIn():
-    return render_template('signIn.html')
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
 
 
-@app.route('/signUp')
-def signUp():
-    return render_template('signUp.html')
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
 
 
 if __name__ == '__main__':
