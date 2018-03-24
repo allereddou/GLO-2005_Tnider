63415e3f7271ed48c401c21b9b355c9efdefb916
Édouard Carré <edouard.carre.1@ulaval.ca>
2018-03-24 01:07:18 -0400

    Avancement, kinda working, needs cleaning

--- data.py
+++ data.py
@@ -33,9 +33,6 @@ cursor = db.cursor(pymysql.cursors.DictCursor)
 sql = "DROP TABLE user"
 cursor.execute(sql)
 
-sql = "DROP TABLE animal"
-cursor.execute(sql)
-
 sql = "DROP TABLE dog"
 cursor.execute(sql)
 
@@ -45,25 +42,34 @@ cursor.execute(sql)
 sql = "DROP TABLE cat"
 cursor.execute(sql)
 
+sql = "DROP TABLE animal"
+cursor.execute(sql)
+
 sql = "CREATE TABLE IF NOT EXISTS user (username VARCHAR(20), pass VARCHAR(100), nom VARCHAR(20), prenom VARCHAR(20), email VARCHAR(40), telephone BIGINT, solde DECIMAL(5,2), PRIMARY KEY (username) );"
 cursor.execute(sql)
 
-sql = "CREATE TABLE IF NOT EXISTS animal (id INT AUTO_INCREMENT PRIMARY KEY , nom VARCHAR(20), sexe CHAR(1), age INT, poids INT, location VARCHAR(20), race VARCHAR(20), description VARCHAR(1000));"
+sql = "CREATE TABLE IF NOT EXISTS animal (id INT PRIMARY KEY , nom VARCHAR(20), sexe CHAR(1), age INT, poids INT, location VARCHAR(20), race VARCHAR(20), description VARCHAR(1000));"
 cursor.execute(sql)
 
-sql = "CREATE TABLE IF NOT EXISTS bird(id INT, PRIMARY KEY (id), plumage VARCHAR(20), FOREIGN KEY (id) REFERENCES (animal) ON DELETE CASCADE);"
+sql = "CREATE TABLE IF NOT EXISTS bird(id INT, PRIMARY KEY (id), plumage VARCHAR(20), FOREIGN KEY (id) REFERENCES animal(id) ON DELETE CASCADE);"
 cursor.execute(sql)
 
-sql = "CREATE TABLE IF NOT EXISTS dog(id INT, PRIMARY KEY (id), pelage VARCHAR(20), castre BOOLEAN, degriffe BOOLEAN,   FOREIGN KEY (id) REFERENCES (animal) ON DELETE CASCADE);"
+sql = "CREATE TABLE IF NOT EXISTS dog(id INT, PRIMARY KEY (id), pelage VARCHAR(20), castre BOOLEAN, degriffe BOOLEAN, FOREIGN KEY (id) REFERENCES animal(id) ON DELETE CASCADE);"
+cursor.execute(sql)
+
+sql = "CREATE TABLE IF NOT EXISTS cat(id INT, PRIMARY KEY (id), pelage VARCHAR(20), castre BOOLEAN, degriffe BOOLEAN, FOREIGN KEY (id) REFERENCES animal(id) ON DELETE CASCADE);"
 cursor.execute(sql)
 
 
 genders = 'mf'
 cities = ['Montreal', 'Quebec', 'Rouyn-Noranda', 'Levis', 'St-Hyacinthe', 'Riviere-Du-Loup', 'London', 'Mongolie Orientale']
 races = ['Birb', 'Doggo', 'Kitteh']
+plumages = ['blanc', 'noir', 'jaune']
+pelages = ['blanc', 'noir', 'brun', 'gris']
+binaryChoice = [0, 1]
 
 # Insérer des users
-for i in range(0, 501):
+for i in range(0, 101):
     password = random.choice(genders)
     hash = hash_password(password)
     gender = random.choice(genders)
@@ -84,7 +90,7 @@ for row in cursor:
     print(row)
 
 # Insérer des animaux
-for j in range(0, 501):
+for j in range(0, 101):
     sexe = random.choice(genders)
     prenom = names.get_first_name(sexe)
     age = random.randint(0, 11)
@@ -93,14 +99,29 @@ for j in range(0, 501):
     race = random.choice(races)
     description = "Hello there aaaaaaaaaaaaaaaaaa"
 
-    sql = "INSERT INTO animal(nom, sexe, age, poids, location, race, description) VALUES ('{}', '{}', {}, {}, '{}', '{}', '{}')"
-    cursor.execute(sql.format(prenom, sexe, age, poids, location, race, description))
+    sql = "INSERT INTO animal(id, nom, sexe, age, poids, location, race, description) VALUES ({}, '{}', '{}', {}, {}, '{}', '{}', '{}')"
+    cursor.execute(sql.format(j, prenom, sexe, age, poids, location, race, description))
 
     if race == 'Birb':
-        sql = "INSERT INTO bird(id, plumage)"
+        plumage = random.choice(plumages)
+        sql = "INSERT INTO bird(id, plumage) VALUES ({},'{}')"
+        cursor.execute(sql.format(j, plumage))
+    elif race == 'Doggo':
+        pelage = random.choice(pelages)
+        castre = random.choice(binaryChoice)
+        degriffe = random.choice(binaryChoice)
+        sql = "INSERT INTO dog(id, pelage, castre, degriffe) VALUES({}, '{}', {}, {})"
+        cursor.execute(sql.format(j, pelage, castre, degriffe))
+    else:
+        pelage = random.choice(pelages)
+        castre = random.choice(binaryChoice)
+        degriffe = random.choice(binaryChoice)
+        sql = "INSERT INTO cat(id, pelage, castre, degriffe) VALUES({}, '{}', {}, {})"
+        cursor.execute(sql.format(j, pelage, castre, degriffe))
 
 
-   
+
+
 
 
 sql = "SELECT * FROM animal"
@@ -108,7 +129,31 @@ cursor.execute(sql)
 
 for row in cursor:
     print(row)
+print("\n + \n")
 
 
 
+sql = "SELECT * FROM bird"
+cursor.execute(sql)
 
+for row in cursor:
+    print(row)
+print("\n + \n")
+
+
+
+sql = "SELECT * FROM dog"
+cursor.execute(sql)
+
+for row in cursor:
+    print(row)
+print("\n + \n")
+
+
+
+sql = "SELECT * FROM cat"
+cursor.execute(sql)
+
+for row in cursor:
+    print(row)
+print("\n + \n")
