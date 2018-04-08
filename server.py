2e2675892bb3beb089c59fde974e663bb9b570b7
gamachexx <olivier.gamache.3@ulaval.ca>
2018-04-08 14:48:57 -0400

    ACCOUNTS

--- server.py
+++ server.py
@@ -28,6 +28,18 @@ def sign_in():
 def contact_us():
     return render_template('contact_us.html')
 
+@app.route('/account-preferences')
+def account_preferences():
+    return render_template('account-preferences.html')
+
+@app.route('/account-transactions')
+def account_transactions():
+    return render_template('account-transactions.html')
+
+@app.route('/account-info')
+def account_info():
+    return render_template('account-info.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
